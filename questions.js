73eca98b9e31ab1c1538e67a8912465cfad5ac37
oseivale <valerie.osei@gmail.com>
2018-12-27 01:03:15 -0500

    created simple quiz app to be used for larger js app

---
+++ questions.js
@@ -0,0 +1,37 @@
+var questions = [{
+	"question": "The first mechanical computer designed by Charles Babbage was called?",
+	"option1": "Abacus",
+	"option2": "Analytical Engine",
+	"option3": "Calculator",
+	"option4": "Processor",
+	"answer": "2"
+}, {
+	"question": "What is the capital city of Ghana?",
+	"option1": "Toronto",
+	"option2": "New York City",
+	"option3": "Accra",
+	"option4": "Kinshasa",
+	"answer": "3"
+
+}, {
+	"question": "What is the most popular sport in the world?",
+	"option1": "Hockey",
+	"option2": "Snowboarding",
+	"option3": "Golf",
+	"option4": "Soccer",
+	"answer": "4"
+}, {
+	"question": "Where is the Empire State Building located?",
+	"option1": "New York City",
+	"option2": "Russia",
+	"option3": "Madagascar",
+	"option4": "Texas",
+	"answer": "1"
+}, {
+	"question": "Who wrote Homegoing?",
+	"option1": "Luke Skywalker",
+	"option2": "Yaa Gyasi",
+	"option3": "Homer Simpson",
+	"option4": "Chenua Achebe",
+	"answer": "2"
+}]
